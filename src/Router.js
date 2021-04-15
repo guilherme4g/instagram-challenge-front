4aea94fcb8e554088ee793ff6434b8333dbd8127
jose guilherme <jgnvs6@gmail.com>
2021-04-14 23:05:32 -0300

    create privacyPolicy component

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 export default function Router() {   
     return (
         <BrowserRouter>
             <Route path="/" exact component={Home} />
+            <Route path="/privacy" exact component={PrivacyPolicy} />
         </BrowserRouter>
     )
 }
