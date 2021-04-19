109d60b826d2537a43dfa5f2357dac0068be6060
jose guilherme <jgnvs6@gmail.com>
2021-04-18 23:42:49 -0300

    feat: create loading to wait response and message if error

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import {  Redirect, useLocation } from "react-router-dom";
+import { useState } from "react";
+import {  Redirect } from "react-router-dom";
 
 import api from "../../api/api";
 
@@ -9,31 +9,45 @@ function Home(props) {
 
   const [token, setToken] = useState('');
   const [profile, setProfile] = useState(null);
-
-  function useQuery() {
-    return new URLSearchParams(useLocation().search);
-  }
-
-  let query = useQuery();
-
-  useEffect(() => {
-    console.log(query.get("code"));
-  });
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const onChangeInput = (value) => {
     setToken(value);
   }
 
+  const handleError = (message) => {
+    setErrorMessage(message);
+      setError(true);
+      setTimeout(() => {
+        setErrorMessage("");
+        setError(false);
+        setLoading(false);
+      }, 5000);
+  }
+
   const onClickButton = () => {
+    setLoading(true);
+    if (token.trim() === "") {
+      handleError("O campo para inserir o token está vazio");
+      setLoading(false);
+      return
+    }
+
     api.get('/user/data/instagram', {
       params: {
         token: token
       }
     }).then(res => {
       setProfile(res.data);
+      setLoading(false);
     }).catch(error => {
-      console.log(error);
+      handleError("Erro ao utilizar o token, verifique se ele está correto");
+      setLoading(false);
     })
+
+    
   }
 
   if (profile) {
@@ -41,6 +55,26 @@ function Home(props) {
   } else {
     return (
       <div className="home-page-wrapper">
+        {
+          (loading) ? (
+            <div class="ui segment">
+              <div class="ui active inverted dimmer">
+                <div class="ui text loader">Loading</div>
+              </div>
+              <p></p>
+            </div>
+          ) : null
+        }
+        {
+          (error) ? (
+            <div class="ui negative message">
+              <i class="close icon"></i>
+              <div class="header">
+                { errorMessage }
+              </div>
+            </div>
+          ) : null
+        }
         <label htmlFor=""> Inserir token: </label>
         <input onChange={e => onChangeInput(e.target.value)} value={token} type="text"/>
         <button onClick={onClickButton}>
@@ -52,3 +86,4 @@ function Home(props) {
 }
 
 export default Home;
+
